8842e33434a2e1c60f788a093679d1bb5d1b7e3d
jadsongouveia <jadsongouveia12@gmail.com>
2024-07-12 19:00:50 -0300

    novos aprendizados

---
+++ src/aula08.ts
@@ -0,0 +1,16 @@
+let dados={
+    nome:"Douglidtilddes" ,
+    idade:25,
+    status:"A",
+    ola:()=>{console.log("oi")},
+    info:(p:string)=>{console.log(p)}
+
+}
+
+dados.nome="Jadson";
+
+console.log(typeof(dados));
+console.log(dados.nome);
+
+dados.ola();
+dados.info(dados.nome);
