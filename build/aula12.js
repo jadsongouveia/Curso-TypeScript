5eca0843030b02217f93c24c7092dea1c5b21009
jadsongouveia <jadsongouveia12@gmail.com>
2024-07-16 20:44:54 -0300

    novos aprendizados

--- build/aula12.js
+++ build/aula12.js
@@ -1,4 +1,11 @@
 "use strict";
-function logar(users) {
-    console.log("Teste");
+function logar(user, pass) {
+    console.log(`User..:${user}`);
+    console.log(`Senha:${pass}`);
 }
+logar("bruno", "123");
+function soma2(n1, n2) {
+    return n1 + n2;
+}
+let res = soma2(10, 3.5);
+console.log(res);
--- src/aula12.ts
+++ src/aula12.ts
@@ -1,3 +1,14 @@
-function logar(user:string):void{
-    console.log("Teste");
+function logar(user:string, pass:string):void{
+    console.log(`User..:${user}`);
+    console.log(`Senha:${pass}`);
 }
+
+logar("bruno","123");
+
+function soma2(n1:number,n2:number):number{
+    return n1+n2;
+}
+
+let res:number=soma2(10,3.5)
+console.log(res)
+
