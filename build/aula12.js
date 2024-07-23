c52fc7bdedb9dc1db3949190c152d3ae16b16f86
jadsongouveia <jadsongouveia12@gmail.com>
2024-07-23 19:12:30 -0300

    novos aprendizados

--- build/aula12.js
+++ build/aula12.js
@@ -1,11 +1,22 @@
 "use strict";
 function logar(user, pass) {
-    console.log(`User..:${user}`);
-    console.log(`Senha:${pass}`);
+    console.log(`User..${user}`);
+    console.log(`Senha...${pass}`);
 }
-logar("bruno", "123");
-function soma2(n1, n2) {
-    return n1 + n2;
+logar('bruno', '123');
+function soma(n1 = 0, n2 = 0) {
+    let r = n1 + n2;
+    return r;
 }
-let res = soma2(10, 3.5);
-console.log(res);
+let n_res = soma(10, 10);
+let s_res = soma(8, 5).toString();
+console.log(n_res);
+console.log(s_res);
+function fatorial(n) {
+    let fat = 1;
+    for (let c = n; c > 1; c--) {
+        fat *= c;
+    }
+    return fat;
+}
+console.log(fatorial(5));
--- src/aula12.ts
+++ src/aula12.ts
@@ -1,14 +1,23 @@
-function logar(user:string, pass:string):void{
-    console.log(`User..:${user}`);
-    console.log(`Senha:${pass}`);
+function logar(user:string,pass:string){
+       console.log(`User..${user}`);
+       console.log(`Senha...${pass}`) 
 }
+logar('bruno','123');
 
-logar("bruno","123");
-
-function soma2(n1:number,n2:number):number{
-    return n1+n2;
+function soma(n1=0,n2=0):number{
+    let r = n1+n2
+    return r
 }
+let n_res= soma(10,10)
+let s_res:string= soma(8,5).toString();
+console.log(n_res)
+console.log(s_res)
 
-let res:number=soma2(10,3.5)
-console.log(res)
-
+function fatorial(n:number) {
+    let fat = 1
+    for(let c = n; c > 1; c--) {
+        fat *= c
+    }
+    return fat
+}
+console.log(fatorial(5))
